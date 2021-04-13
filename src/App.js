fac72a87cfa76c343492a9eb9e2840960180b4db
MIltonFernandez <miltonfernandez1254@hotmail.com>
2021-04-13 04:42:52 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Particles from "react-particles-js";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+import Album from "./components/album";
+import About from "./components/About";
+import Contacto from "./components/Contact";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <>
+    <Switch>
+
+    <Route path = "/Proyectos">
+          <Navbar />
+          <Album/>
+    </Route>
+
+    <Route path = "/About">
+          <Navbar />
+          <About/>
+    </Route>
+
+    <Route path = "/Contacto">
+          <Navbar />
+          <Contacto/>
+    </Route>
+    
+
+    <Route path = "/">
+          <Particles
+            className="particles-canvas"
+            params={{
+            particles: {
+              number: {
+                value: 30,
+                density: {
+                  enable: true, 
+                  value_area: 900,
+                }}, 
+              shape: {
+                type: "circle", 
+                stroke: {
+                  width: 6, 
+                  color: "#f9ab00"
+                }
+              }
+              }
+            }} />
+          <Navbar />
+          <Header/>
+          
+          
+    </Route>
+
+    
+    
+
+    </Switch>
+    </>
+    </Router>
   );
+
+  
 }
+
+
 
 export default App;
